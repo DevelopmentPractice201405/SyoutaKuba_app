b9f27e18463fa2b017d8a0505fbc37d420e06973
syouta <syouta_k-12a@stu.kbc.ac.jp>
2014-08-06 08:39:42 +0900

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,20 +4,20 @@ describe "User pages" do
 
   subject { page }
 
-   describe "signup page" do
-    before { visit signup_path }
+    describe "signup page" do
+      before { visit signup_path }
 
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: 'Sign up') }
-  end
+      it { should have_selector('h1',    text: 'Sign up') }
+      it { should have_selector('title', text: 'Sign up') }
+    end
 
-  describe "profile page" do
-  let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
+    describe "profile page" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { visit user_path(user) }
 
-  it { should have_selector('h1',    text: user.name) }
-  it { should have_selector('title', text: user.name) }
-end
+      it { should have_selector('h1',    text: user.name) }
+      it { should have_selector('title', text: user.name) }
+    end
 
   describe "signup" do
 
@@ -38,9 +38,15 @@ end
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
+        describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
 
-
-
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+      
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
